a13f9e40dd1255f8a0860711ce65f3c1d4d8057c
jazzdhaobe <115687960+jazzdhaobe@users.noreply.github.com>
2024-01-28 17:13:47 +0530

    Add files via upload

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyByUvZ21pmSEktSCc2hnRojT1Pjt1vxhQ0",
+  authDomain: "clonzy-d624e.firebaseapp.com",
+  projectId: "clonzy-d624e",
+  storageBucket: "clonzy-d624e.appspot.com",
+  messagingSenderId: "217204022670",
+  appId: "1:217204022670:web:507d7b4486c81945a6d8df",
+  measurementId: "G-PK9TNQ3RQW"
+};
+
+export default firebaseConfig;
